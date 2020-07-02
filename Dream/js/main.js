58980c06b560f3fd1ce034717f48f1c81387b4f7
makedon4eg <makedon4eg@gmail.com>
2020-07-02 19:57:12 +0300

    FirstProject

---
+++ Dream/js/main.js
@@ -0,0 +1,13 @@
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.wish').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__backInLeft");
+	        }
+	    });
+	});
+})
